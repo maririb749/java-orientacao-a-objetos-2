1a132a2e0f8461b5cadac034314c9d81bc8cb295
Mariana Santos <maririb51@gmail.com>
2022-08-10 12:50:38 +0100

    Interface Compareble

--- src/application/Program8.java
+++ src/application/Program8.java
@@ -7,33 +7,36 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
+
+import entities.Employees;
 
 public class Program8 {
 
 	/*
-	 * Faça um programa para ler um arquivo contendo nomes de pessoas(um nome por
-	 * linha), armazenando-os em uma lista.Depois, ordenar os dados dessa lista e
-	 * mostra-los ordenadamente na tela. Nota: o caminho do arquivo pode ser
-	 * informado "hardcode".
+	 * Faça um programa para ler um arquivo contendo funcionários( nomee salário),no
+	 * formato .csv,armazenando-os em uma lista. Depois, ordenar a lita por nome e
+	 * mostrar o resultado na tela. Nota: o caminho do arquivo pode ser informado
+	 * "hardcode".
 	 */
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 
-		List<String> list = new ArrayList();
+		List<Employees> list = new ArrayList();
 		String path = "C:\\temp\\in.txt";
 
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
-			String name = br.readLine();
-			while (name != null) {
-				list.add(name);
-				name = br.readLine();
+			String employeesCsv = br.readLine();
+			while (employeesCsv != null) {
+				String[]fields = employeesCsv.split(",");
+				list.add(new Employees(fields[0], Double.parseDouble(fields[1])));
+				employeesCsv = br.readLine();
 			}
 			Collections.sort(list);
-			for (String s : list) {
-				System.out.println(s);
+			for (Employees emp : list) {
+				System.out.println(emp.getName()+ " , " + emp.getSalary());
 			}
+			
 		} catch (IOException e) {
 			System.out.println("Error:" + e.getMessage());
 		}
