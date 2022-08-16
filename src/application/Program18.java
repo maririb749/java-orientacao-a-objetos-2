9f3a228d9c1e2b93871e0ad7d55109b64c1cb24d
Mariana Santos <maririb51@gmail.com>
2022-08-16 15:00:22 +0100

    Function-Expressão lambda declarada

--- src/application/Program18.java
+++ src/application/Program18.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
  
 import entities.Product5;
@@ -22,8 +23,10 @@ public class Program18 {
 		list.add(new Product5("Mouse", 50.00));
 		list.add(new Product5("Tablet", 350.50));
 		list.add(new Product5("HD Case", 80.00));
+		
+		Function<Product5, String> func = p -> p.getName().toUpperCase();
 
-		List<String> name = list.stream().map(Product5::nonstaticUpperCaseName).collect(Collectors.toList());
+		List<String> name = list.stream().map(func).collect(Collectors.toList());
 		
 		name.forEach(System.out::println);
 
